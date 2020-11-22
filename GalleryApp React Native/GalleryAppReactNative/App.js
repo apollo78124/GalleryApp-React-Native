d8b9eb4f6b230522cc1bf37e2ee3283fb2fd7636
apollo78124 <98apollo@naver.com>
2020-11-22 13:40:55 -0800

    Scroll bug fix

--- GalleryApp React Native/GalleryAppReactNative/App.js
+++ GalleryApp React Native/GalleryAppReactNative/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image, Button, Alert } from 'react-native';
+import GestureRecognizer, {swipeDirections} from 'react-native-swipe-gestures';
 import PresentationalComponent from './PresentationalComponent'
 export default class App extends React.Component {
    
@@ -11,7 +12,8 @@ export default class App extends React.Component {
         dir: "./assets/",
         currentPosition: 0,
         photoList: [require('./assets/8b52.jpg'), require('./assets/alex.jpg'), require('./assets/fox.jpg'), require('./assets/image_02af.jpg'), require('./assets/image_13a.jpg'), require('./assets/image_43e.jpg'), require('./assets/image_49aba.jpg'), require('./assets/image_db0cc.jpg')],
-        myState: require('./assets/8b52.jpg')
+        myState: require('./assets/8b52.jpg'),
+        myText: 'I\'m ready to get swiped!',
       }; // On Android, use "RNFS.DocumentDirectoryPath" (MainBundlePath is not defined)
 
       console.log('--------------Beginning of List------------');
@@ -19,7 +21,28 @@ export default class App extends React.Component {
       console.log('--------------End of List-------------------');
       
     }
-   
+    onSwipeLeft(gestureState) {
+      this.setState({myText: 'You swiped left!'});
+      this.imageLeft();
+    }
+  
+    onSwipeRight(gestureState) {
+      this.setState({myText: 'You swiped right!'});
+      this.imageRight();
+    }
+
+    onSwipe(gestureName, gestureState) {
+      const {SWIPE_UP, SWIPE_DOWN, SWIPE_LEFT, SWIPE_RIGHT} = swipeDirections;
+      this.setState({gestureName: gestureName});
+      switch (gestureName) {
+        case SWIPE_LEFT:
+          this.setState({myText: 'You swiped left!'});
+          break;
+        case SWIPE_RIGHT:
+          this.setState({myText: 'You swiped right!'});
+          break;
+      }
+    }
    updateState = () => {
       this.state = { myState: 'The state is updated' };
    }
@@ -43,19 +66,31 @@ export default class App extends React.Component {
   }
 
    render() {
+      const config = {
+        velocityThreshold: 0.3,
+        directionalOffsetThreshold: 80
+      };
       return (
          <View>
-            <PresentationalComponent myState = {this.state.myState} updateState = {this.updateState}/>
-              <Image style = {{width: 450, height: 300}} source = { this.state.myState} />
-              <Button
-                title="Left"
-                onPress={this.imageLeft}
-              />
-              <Button
-                title="Right"
-                onPress={this.imageRight}
-              />
-			         </View>
+          <GestureRecognizer
+              onSwipe={(direction, state) => this.onSwipe(direction, state)}
+              onSwipeLeft={(state) => this.onSwipeLeft(state)}
+              onSwipeRight={(state) => this.onSwipeRight(state)}
+              config={config}
+            >
+              <PresentationalComponent myState = {this.state.myState} updateState = {this.updateState}/>
+                <Image style = {{width: 450, height: 300}} source = { this.state.myState} />
+                <Button
+                  title="Left"
+                  onPress={this.imageLeft}
+                />
+                <Button
+                  title="Right"
+                  onPress={this.imageRight}
+                />
+                  <Text>{this.state.myText}</Text>
+              </GestureRecognizer>
+			      </View>
 		
       );
    }
